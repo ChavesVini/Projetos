44e92bd0924170ce1d6c9a294b2c1024ec786876
Vinícius Chaves <126925449+ChavesVini@users.noreply.github.com>
2023-08-06 23:17:52 -0300

    programming logic with java

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        String nome = "Vinícius";
+        String sobrenome = "Chaves";
+        int idade = 18;
+        Double altura = 1.80;
+        char sexo = 'M';
+        System.out.println("Nome: " + nome);
+        System.out.println("Sobrenome: " + sobrenome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Sexo: " + sexo);
+    }
+}
+
+//String = texto, identificando com aspas duplas
+//int = números inteiros
+//double = números quebrados como decimais.
+//float = mesma coisa do double, somente tendo que identificar com F ou f depois do número.
+//char = somente aceita uma letra, sendo identificado com aspas simples
---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+}
+}
+
+//println pula uma linha depois da mensagem escrita;
+//print apenas escreve e o próximo comando fica na mesma linha;
+//NUNCA SE ESQUEÇA DO PONTO E VIRGULA APÓS CADA LINHA DE CÓDIGO;
+//Além das chaves em classes e packages;
+//se escrever sysout e apertar tab, aparecerá esse comando System.out.println(), é como se fosse o print em Python;
+//mesma coisa acontece se escrever main, aparecerá public static void main(String[] args).
