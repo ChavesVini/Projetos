b7fbd2f3ba9256c1398c62eaf01f25c7c58d7971
Vinícius Chaves <126925449+ChavesVini@users.noreply.github.com>
2023-10-27 22:27:04 -0300

    Add files via upload

---
+++ Nova pasta/ToDoModel.java
@@ -0,0 +1,66 @@
+package com.tg.manager.model;
+
+import com.tg.manager.model.connection.ConnectionDataBase;
+import lombok.Data;
+import lombok.ToString;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@ToString
+public class ToDoModel {
+    private String feedback;
+    private Double note;
+    private Integer idStudent;
+    private Integer idIssue;
+
+    public void addToDo(String feedback, Double note, Integer idStudent, Integer idIssue) {
+        try {
+            ConnectionDataBase connectionDb = new ConnectionDataBase();
+            Connection connection = connectionDb.getConexao();
+            String insercaoSQL = "INSERT INTO valor_entrega (nota, feedback, idaluno, identrega) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insercaoSQL);
+            preparedStatement.setDouble(1, note);
+            preparedStatement.setString(2, feedback);
+            preparedStatement.setInt(3, idStudent);
+            preparedStatement.setInt(4, idIssue);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            //connection.close();
+            System.out.println("Dados inseridos com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public Set<ToDoModel> getToDo() throws SQLException {
+        try {
+            ConnectionDataBase connectionDb = new ConnectionDataBase();
+            Connection connection = connectionDb.getConexao();
+            Statement statementDb = connection.createStatement();
+            ResultSet result = statementDb.executeQuery("SELECT * from valor_entrega");
+            Set<ToDoModel> toDoList = new HashSet<>();
+            while (result.next()) {
+                ToDoModel toDo = new ToDoModel();
+                Double grade = result.getDouble("nota");
+                toDo.setNote(grade);
+                String feedback = result.getString("feedback");
+                toDo.setFeedback(feedback);
+                Integer idAluno = result.getString("idaluno");
+                toDo.setIdStudent(idAluno);
+                String idEntrega = result.getString("identrega");
+                toDo.setIdIssue(idEntrega);
+                toDoList.add(toDo);
+            }
+            return toDoList;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+}
